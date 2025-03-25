e3d81bbeaad4daded311a4f7729bf5978141818b
Subrahmanyam G <subrahmanyam_g@Subrahmanyams-MacBook-Air.local>
2025-03-25 23:07:22 +0530

    auto deduction of return type using type traits in templates

---
+++ basics/include/utils.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <type_traits>
+
+// auto deduction of return type
+template <typename U, typename V>
+auto area(U a, V b) {               // Type is automatically decuded to double.
+    return a*b;
+}
+
+
+template <typename U, typename V>
+std::common_type_t<U,V> area_double(U a, V b) {               // Type Traits.
+    return a*b;
+}
---
+++ basics/main.cpp
@@ -0,0 +1,8 @@
+#include "utils.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Area of rectangle: " << area(10, 20.5) << std::endl;
+    std::cout << "Area of rectangle: " << area_double(10, 20.5) << std::endl;
+    return 0;
+}
