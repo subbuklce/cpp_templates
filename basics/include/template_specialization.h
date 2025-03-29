5ff7eafbd3791100496dd6179875da818a752dcb
Subrahmanyam Gundimeda <subbu.klce@outlook.com>
2025-03-29 07:25:53 +0530

    Explicit Template functions
    Difference between explicit template specialization vs normal template function

---
+++ basics/include/template_specialization.h
@@ -0,0 +1,33 @@
+#include<iostream>
+
+template<typename T>
+T Max(T a, T b) {
+    return (a > b) ? a : b;
+}
+template<>
+char* Max(char* a, char* b) {
+    std::cout << "Specialized version for char*" << std::endl;
+    return (strcmp(a, b) > 0) ? a : b;
+}
+template<typename T>
+T* Max(T* a, T* b) {
+    std::cout << "Specialized version for pointers" << std::endl;
+    return (*a > *b) ? a : b;
+}
+
+
+
+//Notes 
+/*
+If char* Max is declared first then it is not treated as a specialization of the template function Max<T>.
+The compiler will treat it as a separate function.
+The specialization must be declared after the primary template function. And the primary template function 
+for char* Max is third function.
+
+char* a = "C++";
+char* b = "Java";
+cout<<Max(a, b)<<endl; // This will call the specialized version for pointers
+
+swap char* Max & T* Max and see the magic.
+
+*/
--- basics/main.cpp
+++ basics/main.cpp
@@ -1,8 +1,15 @@
+#include "template_specialization.h"
 #include "utils.h"
-#include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Area of rectangle: " << area(10, 20.5) << std::endl;
-    std::cout << "Area of rectangle: " << area_double(10, 20.5) << std::endl;
-    return 0;
+
+  // std::cout << "Area of rectangle: " << area(10, 20.5) << std::endl;
+  // std::cout << "Area of rectangle: " << area_double(10, 20.5) << std::endl;
+
+  char *a = "C++";
+  char *b = "Java";
+  cout << Max(a, b) << endl;
+
+  return 0;
 }
